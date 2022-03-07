59efbdbea3b802a0fc31992753a82c0dce5a00ec
Fin <fantomma91@gmail.com>
2022-03-07 22:05:30 +0700

    refact

--- main.js
+++ main.js
@@ -1,20 +1,12 @@
 import {
-  GENDER_UI
+  GENDER_UI,
+  createUrl,
+  getJson,
+  renderResult,
+  showError,
+  showResult,
 } from "./view.js";
 
-const createUrl = name => `${GENDER_UI.API_URL}?name=${name.value.trim()}`;
-const getJson = url => fetch(url).then(response => response.json());
-const renderResult = (name, gender) => (GENDER_UI.RESULT.value = `${name.value} - ${gender}`)
-const showError = (err) => GENDER_UI.ERROR.textContent = err.message;
-
-function showResult(json) {
-  if (json.gender) {
-    GENDER_UI.ERROR.textContent = ''
-    return renderResult(GENDER_UI.NAME, json.gender)
-  }
-  GENDER_UI.RESULT.value = ''
-  throw new Error('Имя отсутствует или некорректное');
-}
 
 GENDER_UI.FORM.addEventListener('submit', function (e) {
   e.preventDefault()
--- view.js
+++ view.js
@@ -6,3 +6,17 @@ export const GENDER_UI = {
   RESULT: document.querySelector('.gender__result'),
   ERROR: document.querySelector('.gender__error'),
 }
+
+export const createUrl = name => `${GENDER_UI.API_URL}?name=${name.value.trim()}`;
+export const getJson = url => fetch(url).then(response => response.json());
+export const renderResult = (name, gender) => (GENDER_UI.RESULT.value = `${name.value} - ${gender}`)
+export const showError = (err) => GENDER_UI.ERROR.textContent = err.message;
+
+export function showResult(json) {
+  if (json.gender) {
+    GENDER_UI.ERROR.textContent = ''
+    return renderResult(GENDER_UI.NAME, json.gender)
+  }
+  GENDER_UI.RESULT.value = ''
+  throw new Error('Имя отсутствует или некорректное');
+}
